3bea40b60ced73a77b73d111b604b66be51f7b96
knolukas <knogler.lukas@gmail.com>
2024-05-14 15:30:39 +0200

    ergänzungen

--- vns.jl
+++ vns.jl
@@ -22,77 +22,83 @@ end
 #maximale Anzahl geöffneter Factories
 max_fac = 4
 
-# Build Solution 1
-function build_solution1(n::Int64, m::Int64, c::Matrix{Int64}, o::Vector{Int64}, max_fac::Int64)
+# # Build Solution 1
+# function build_solution1(n::Int64, m::Int64, c::Matrix{Int64}, o::Vector{Int64}, max_fac::Int64)
     
-    #Initialize decision variable with zeros
-    #create model
-    model = Model(CPLEX.Optimizer)
-    set_silent(model)
-    #make the variables
-    @variable(model, x[1:n, 1:m], Bin)
-    @variable(model, y[1:m], Bin)
+#     #Initialize decision variable with zeros
+#     #create model
+#     model = Model(CPLEX.Optimizer)
+#     set_silent(model)
+#     #make the variables
+#     @variable(model, x[1:n, 1:m], Bin)
+#     @variable(model, y[1:m], Bin)
 
-    #create objective function
-    @objective(model, Min, sum(c[i,j]*x[i,j] for i in 1:n for j in 1:m) + sum(o[j] * y[j] for j in 1:m))
+#     #create objective function
+#     @objective(model, Min, sum(c[i,j]*x[i,j] for i in 1:n for j in 1:m) + sum(o[j] * y[j] for j in 1:m))
 
-    #create constraints
-    @constraint(model, open_facilities[j = 1:m], sum(x[i,j] for i in 1:n) <= y[j]*n)
-    @constraint(model, max_facilities, sum(y[j] for j in 1:m) <= max_fac)
-    # every customer gets served by one factory
-    @constraint(model, cust_serv[i = 1:n], sum(x[i,j] for j in 1:m) == 1)
-    optimize!(model)
-    return model, x , y
-end
-model, x, y  = build_solution1(n, m, c, opening_costs, max_fac)
-value.(x)
-value.(y)
+#     #create constraints
+#     @constraint(model, open_facilities[j = 1:m], sum(x[i,j] for i in 1:n) <= y[j]*n)
+#     @constraint(model, max_facilities, sum(y[j] for j in 1:m) <= max_fac)
+#     # every customer gets served by one factory
+#     @constraint(model, cust_serv[i = 1:n], sum(x[i,j] for j in 1:m) == 1)
+#     optimize!(model)
+#     return model, x , y
+# end
+# model, x, y  = build_solution1(n, m, c, opening_costs, max_fac)
+# value.(x)
+# value.(y)
 
 # Build Solution 2
 function build_solution2(n::Int64, m::Int64, c::Matrix{Int64}, max_fac::Int64)
     #choose randomly which factories are opened
-    y = randperm(10)[1:max_fac]
-    x = zeros(Int,n,m)
+    objective_value = randperm(10)[1:max_fac]
+    solution = zeros(Int,n,m)
     #always assign customer to cheapest opened facility
     for i in 1:n
         index = argmin(c[i, y])
         index = y[index]
         x[i,index] =1
     end
-    return x, y
+    return solution, objective_value
 end
 
 x2, y2 = build_solution2(n, m, c, max_fac)
 x2
+y2
 
 # Neighbourhood search
 
-# Kostenfunktion für das Routing-Problem (hier als Beispiel die Summe der Distanzen zwischen den Orten)
-function cost(solution)
-    total_distance = 0
-    for i in 1:length(solution)-1
-        total_distance += c[solution[i], solution[i+1]]
+# Kostenfunktion (Mulitplikation von Opening Matrix and Cost Matrix)
+function generate_objective_value(solution::Matrix{Int64})
+    sum = 0
+    for i in 1:100
+        for j in 1:10
+            sum += solution[i,j]*c[i,j]
+        end
     end
-    total_distance += c[solution[end], solution[1]]  # Zurück zum Startpunkt
-    return total_distance
+    return sum #objective value
 end
 
+generate_objective_value(x2)
+
 # Funktion zur Generierung einer Nachbarlösung durch Verschieben eines Ortes in der Tour
-function generate_neighbor(solution)
+function generate_neighbor(solution) #k als parameter einbauen (Anzahl der Moves)
     neighbor = copy(solution)
     idx1, idx2 = rand(1:length(solution), 2)
     neighbor[idx1], neighbor[idx2] = neighbor[idx2], neighbor[idx1]
-    return neighbor
+    return neighbor, k #k als return value
 end
 
+generate_neighbor(x2)
+
 # Einfache Nachbarschaftssuche mit gegebener Initiallösung
-function neighborhood_search(initial_solution, max_iterations)
-    current_solution = initial_solution
-    current_cost = cost(current_solution)
+function local_search(neighbor, max_iterations) # k aus neighborhood einfügen
+    current_solution = neighbor
+    current_cost = generate_objective_value(current_solution)
     
     for i in 1:max_iterations
-        neighbor_solution = generate_neighbor(current_solution)
-        neighbor_cost = cost(neighbor_solution)
+        neighbor_solution, k = generate_neighbor(current_solution) #k einfügen
+        neighbor_cost = generate_objective_value(neighbor_solution)
         
         if neighbor_cost < current_cost
             current_solution = neighbor_solution
@@ -103,22 +109,9 @@ function neighborhood_search(initial_solution, max_iterations)
     return current_solution, current_cost
 end
 
+local_search(x2, 1000)
 
-# Local Seach 1
-function steepest_descent(solution, inital_objective_value)
-    best_solution = solution[1]
-    best_solution_obj_value = inital_objective_value
-
-    for solution in solution[2:end]
-        solution_obj_value = cost(solution)
-        if solution_obj_value < best_solution_obj_value
-            best_solution = solution
-            best_solution_obj_value = solution_obj_value
-        end
-    end
-    
-    return best_solution, best_solution_obj_value
-end
+a = generate_objective_value(x2)
 
 # Local Search 2
 function tabu_search(solution, tabu_tenure, inital_objective_value)
@@ -132,7 +125,7 @@ function tabu_search(solution, tabu_tenure, inital_objective_value)
             continue
         end
         
-        solution_obj_value = cost(neighbor)
+        solution_obj_value = generate_objective_value(neighbor)
         
         if solution_obj_value < best_solution_obj_value
             best_solution = neighbor
@@ -153,16 +146,17 @@ end
 
 # Acceptance decision
 function acceptance_decision(initial_value, updated_value)
-    return (cost(updated_value) > cost(initial_value))
+    return (generate_objective_value(updated_value) > generate_objective_value(initial_value))
 end
 
+max_iterations = 10
 
-function variable_neighborhood_search(build_solution, neighborhood_function, local_search, acceptance_decision, stopping_criterion, z)
-    x = build_solution()
+function variable_neighborhood_search(z)
+    x, y = build_solution2(n, m, c, max_fac)
     x_star = copy(x)  # Julia's `copy` function is used to create a deep copy of the array
     k = 1
 
-    while !stopping_criterion()
+    while count < max_iterations
         x_prime = neighborhood_function(x, k)
         x_double_prime = local_search(x_prime)
 
@@ -176,6 +170,10 @@ function variable_neighborhood_search(build_solution, neighborhood_function, loc
         else
             k = 1 + (k % κ)
         end
+        count = count + 1
     end
     return x_star
 end
+
+
+print(variable_neighborhood_search())
