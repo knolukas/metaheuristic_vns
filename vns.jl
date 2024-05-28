8fe3a248e35824f7e579cceb17fda91cc8cc25b4
ThmsPmmr <thomas.pammer00@gmx.at>
2024-05-28 11:14:36 +0200

    included integration of different number of opened plants

--- vns.jl
+++ vns.jl
@@ -17,13 +17,25 @@ function build_solution()
 end
 
 
+# Build Solution
+function build_solution_2(num_fac)
+    #choose randomly which factories are opened
+    plants = [ones(Int, num_fac); zeros(Int, m - num_fac)]
+    shuffle!(plants)
+    
+    #always assign customer to most prefered opened facility
+    assignment = assignCustomers(plants)
+
+    return assignment, plants
+end
+
 # Funktion zur Generierung einer Nachbarlösung durch Verschieben eines Ortes in der Tour
 function generate_neighbor(plants, k)
     neighbor = copy(plants) 
-    n = length(plants)
+    #n = length(plants)
     
     # Sicherstellen, dass k nicht größer als die Anzahl der Elemente ist
-    k = min(k, div(n, 2))
+    k = min(k, div(m, 2))
 
     for _ in 1:k
         opened = findall(x -> x == 1, neighbor)
@@ -164,11 +176,50 @@ function variable_neighborhood_search()
     return best_assignment, best_plant, best_objective_value
 end
 
+#################################################################################################
+#################################################################################################
+# Variable neighborhood adapated function
+#################################################################################################
+#################################################################################################
+function variable_neighborhood_search_2()
+    assignment, plant = build_solution_2(1)
+    best_assignment = copy(assignment)  # Julia's `copy` function is used to create a deep copy of the array
+    best_plant = copy(plant)
+    best_objective_value = Inf
+    count = 0
+    k = 1
+    
+    for num_facilities in 1:m
+        assignment, plant = build_solution_2(num_facilities)
+        while count < max_iterations
+            plant_neighbor = generate_neighbor(plant, k)
+            assignment_prime, plant_prime, objective_value = local_search(assignment, plant_neighbor)
+    
+            if acceptance_decision(best_assignment, best_plant, assignment_prime, plant_prime)
+                
+                best_assignment = copy(assignment_prime)  
+                best_plant = copy(plant_prime)  # Ensure a deep copy
+                best_objective_value = objective_value
+                # Ensure a deep copy
+                k = 1
+    
+                
+            else
+                k += 1 
+            end
+            count = count + 1
+        end
+    end
+    
+    return best_assignment, best_plant, best_objective_value
+end
+
+
 ######################## Main Script #####################################################
 
 # Initialisieren der Parameter
-n = 5  # Anzahl der Kunden
-m = 5   # Anzahl der Einrichtungen
+n = 20  # Anzahl der Kunden
+m = 10   # Anzahl der Einrichtungen
 
 # Generieren der Kostenmatrix c
 c = rand(1:100, n, m)  # Transportkosten, zufällige positive Integer zwischen 1 und 100
@@ -183,7 +234,9 @@ for i in 1:n
 end
 
 #maximale Anzahl geöffneter Factories und Iterations
-max_fac = 1
-max_iterations = 40
+max_fac = 3
+max_iterations = 10
 
 print(variable_neighborhood_search())
+println("Adapted solution")
+println(variable_neighborhood_search_2())
